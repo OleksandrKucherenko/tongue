092807794f7c7c1a910e19255c61d9d37c947424
Oleksandr Kucherenko <kucherenko.alex@gmail.com>
2015-05-12 22:40:22 +0200

    FIX: mocking of the Log.isLoggable

--- tongue/src/test/java/com/artfulbits/tongue/TongueTests.java
+++ tongue/src/test/java/com/artfulbits/tongue/TongueTests.java
@@ -1,6 +1,8 @@
 package com.artfulbits.tongue;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.artfulbits.benchmark.Meter;
 import com.artfulbits.tongue.web.CacheProvider;
@@ -8,12 +10,16 @@ import com.artfulbits.tongue.web.Language;
 
 import org.junit.*;
 import org.junit.rules.*;
+import org.mockito.Mockito;
 
 import java.security.InvalidParameterException;
 import java.util.logging.Level;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 /** jUnit tests. */
 @SuppressWarnings("PMD")
@@ -81,12 +87,30 @@ public class TongueTests {
   public void test_01_WrongInitialization() {
     Tongue.getCache();
 
-    fail("Disk cache should not be available until the Tongue.iniitialize() is called.");
+    fail("Disk cache should not be available until the Tongue.initialize() is called.");
   }
 
   @Test
   public void test_02_NormalInitialization() throws Exception {
-    final Context context = new android.test.mock.MockContext();
+    final Context context = new android.test.mock.MockContext() {
+      @Override
+      public String getPackageName() {
+        return "com.test";
+      }
+
+      @Override
+      public PackageManager getPackageManager() {
+        final PackageManager pm = Mockito.mock(PackageManager.class);
+        final PackageInfo pi = Mockito.mock(PackageInfo.class);
+
+        try {
+          when(pm.getPackageInfo(anyString(), anyInt())).thenReturn(pi);
+        } catch (final PackageManager.NameNotFoundException ignored) {
+
+        }
+        return pm;
+      }
+    };
 
     Tongue.initialize(context);
     final CacheProvider cache = Tongue.getCache();
